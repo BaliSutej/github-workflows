b7f7cd75b1372be4e832cfa9a788b293e7991f83
sutej bali <sutejbali@gmail.com>
2023-03-04 22:18:58 +0530

    test: added failing test

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -2,4 +2,8 @@ describe("Description of test suit", () => {
 	test("description of test", async () => {
 		expect(2 + 2).toBe(4);
 	});
+
+	test("failing test", async () => {
+		expect(2 + 2).toBe(5);
+	});
 });
